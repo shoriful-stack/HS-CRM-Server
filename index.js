2f54d03f95be1ebac19205287e1bed30d8d24156
Sa18-f <sisazid182713@gmail.com>
2024-10-08 22:51:41 +0600

    another contract get endpoint created for specific contract

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -764,6 +766,14 @@ async function run() {
                 res.status(500).send('Server error');
             }
         });
+
+        // get the specific contract 
+        app.get("/contracts/view/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await contractsCollection.findOne(query);
+            res.send(result)
+        })
 
         // Get 1st 10 contracts with pagination
         app.get("/contracts", async (req, res) => {
